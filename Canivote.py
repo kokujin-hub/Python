4b572d7eaac8f8feb0f4e389e10ab0aae519f054
kokoko <kurtjayalegado@riccarton.school.nz>
2025-03-18 09:19:33 +1300

    nothing

--- Canivote.py
+++ Canivote.py
@@ -1,1 +1,20 @@
+"""This program will print"""
+
+try:
+    while True:
+        num = input('Enter a number: ')
+
+        if num == '-':
+            break
+
+        num1 = int(num)
+
+        if num1.isdecimal:
+            print('boop') 
+        for i in num:
+            print('beep')
+except ValueError:
+    print('not robot')
+
+
 
