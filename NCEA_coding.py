b3a986df6e93fcfef655000face49267ecc669f9
kokoko <kurtjayalegado@riccarton.school.nz>
2025-03-25 09:33:10 +1300

    test

--- NCEA_coding.py
+++ NCEA_coding.py
@@ -3,12 +3,23 @@
 #Variables and lists
 safe_speed = 10
 speedrecord = []
+end_code = 'end'
 
 #infinite loop until a certain word or number is meet
 while True: 
     try: #if error occurs it prints something
-        speed = int(input("Input descent speed in m/s: "))
-        if speed == 'end':
-            break
-        
+        speed = input("Input descent speed in m/s: ") #ask for the speed
+
+        if speed == end_code:
+            break #if input is 'end' the code stops
+        speed = int(speed)
+        elif speed <= 0:
+            print("Error, invalid input.")
+        speed = float(speed) #converting int speed to float speed
+        speedrecord.append(speed) # putting the inputs onto the list
+
+    except ValueError: 
+        print('Error, invalid input.') #if a non number is input it prints this 
+
+
 
