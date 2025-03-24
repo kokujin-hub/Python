221aae8376523af0e036e7ac18ba13e0ef79de40
kokoko <kurtjayalegado@riccarton.school.nz>
2025-03-25 10:16:27 +1300

    I added a code that will count the numbers that are above the safe speed, idk if it will work though

--- NCEA_coding.py
+++ NCEA_coding.py
@@ -1,27 +1,35 @@
-'''This program will record the unsafe speed'''
+'''This program will see look if there is an unsafe speed'''
 
-#Variables and lists
-safe_speed = 10
+#variables and list
+safe_speed = 10 
 speedrecord = []
-end_code = 'end'
+end_code = 'end' #a word that will end the inf loop
 not_safe_speed = 0
+ 
+ #infinite loop until a certain word or number is meet
+while True:
+    try:
+        speed = input("Input descent speed in m/s: ") #asking for the speed
 
-#infinite loop until a certain word or number is meet
-speed = input("Input descent speed in m/s: ")
-
-while speed != end_code: 
-    try: #if error occurs it prints something
-        
-        speed = float(speed) #converting int speed to float speed
-        speedrecord.append(speed) # putting the inputs onto the list        
+        if speed == end_code:
+            break #if input is 'end' the code stops
 
         if int(speed) <= 0: 
             print("Error, invalid input.")  #if 0 or negative number is put it prints out "invalid input".
-        
+
+        speed = float(speed) #converting int speed to float speed
+        speedrecord.append(speed) # putting the inputs onto the list
     except ValueError: 
         print('Error, invalid input.') #if a non number is input it prints this 
 
+speed = float(speed)
+for speed in speedrecord:
+    if speed > safe_speed:
+        not_safe_speed += 1
 
+print(f"There were {not_safe_speed} rockets faster than the safe speed")
+     
+ 
 
 
 
