8a508c2af4ba3ad2c90e060a716e29d2db903a1f
kokoko <kurtjayalegado@riccarton.school.nz>
2025-03-25 09:54:04 +1300

    I changed the condition of the while infinite loop

--- NCEA_coding.py
+++ NCEA_coding.py
@@ -7,24 +7,25 @@ end_code = 'end'
 not_safe_speed = 0
 
 #infinite loop until a certain word or number is meet
-while True: 
-    try: #if error occurs it prints something
-        speed = input("Input descent speed in m/s: ") #ask for the speed
+speed = input("Input descent speed in m/s: ")
 
-        if speed == end_code:
-            break #if input is 'end' the code stops
+while speed != end_code: 
+    try: #if error occurs it prints something
+        
+        speed = float(speed) #converting int speed to float speed
+        speedrecord.append(speed) # putting the inputs onto the list        
+
         if int(speed) <= 0: 
             print("Error, invalid input.")  #if 0 or negative number is put it prints out "invalid input".
         
-        speed = float(speed) #converting int speed to float speed
-        speedrecord.append(speed) # putting the inputs onto the list
-
     except ValueError: 
         print('Error, invalid input.') #if a non number is input it prints this 
 
-for speeds in speedrecord:
-    print(speeds)
-    
+
+
+
+
+
 
 
 
